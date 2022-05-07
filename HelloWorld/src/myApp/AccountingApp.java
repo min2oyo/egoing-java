5187966698d141e0453bee4c386ebe448394a73a
min2oyo <min2u@outlook.com>
2022-05-07 21:52:33 +0900

    14 나의 앱 만들기 | class

--- HelloWorld/src/myApp/AccountingApp.java
+++ HelloWorld/src/myApp/AccountingApp.java
@@ -4,19 +4,17 @@ public class AccountingApp {
 
 	public static void main(String[] args) {
 
-		double valueOfSupply = 10000.0; // 공급가
-
+		double valueOfSupply = 20000.0; // 공급가
+		
 		double vatRate = 0.1; // 부가가치세율
 		double vat = valueOfSupply * vatRate; // 부가가치세
 		double total = valueOfSupply + vat; // 결제액
 		double expenseRate = 0.3; // 비용율
 		double expense = valueOfSupply * expenseRate; // 비용
 		double income = valueOfSupply - expense; // 이익
-
-		double[] dividendRates = { 0.5, 0.3, 0.2 }; // 동업자
-		double dividend1 = income * dividendRates[0];
-		double dividend2 = income * dividendRates[1];
-		double dividend3 = income * dividendRates[2];
+		double dividend1 = income * 0.5; // 동업자 1
+		double dividend2 = income * 0.3; // 동업자 2
+		double dividend3 = income * 0.2; // 동업자 3
 
 		System.out.println("Value of supply: " + valueOfSupply);
 		System.out.println("VAT: " + vat);
--- HelloWorld/src/myApp/AccountingArrayApp.java
+++ HelloWorld/src/myApp/AccountingArrayApp.java
@@ -4,17 +4,19 @@ public class AccountingArrayApp {
 
 	public static void main(String[] args) {
 
-		double valueOfSupply = 20000.0; // 공급가
-		
+		double valueOfSupply = 10000.0; // 공급가
+
 		double vatRate = 0.1; // 부가가치세율
 		double vat = valueOfSupply * vatRate; // 부가가치세
 		double total = valueOfSupply + vat; // 결제액
 		double expenseRate = 0.3; // 비용율
 		double expense = valueOfSupply * expenseRate; // 비용
 		double income = valueOfSupply - expense; // 이익
-		double dividend1 = income * 0.5; // 동업자 1
-		double dividend2 = income * 0.3; // 동업자 2
-		double dividend3 = income * 0.2; // 동업자 3
+
+		double[] dividendRates = { 0.5, 0.3, 0.2 }; // 동업자
+		double dividend1 = income * dividendRates[0];
+		double dividend2 = income * dividendRates[1];
+		double dividend3 = income * dividendRates[2];
 
 		System.out.println("Value of supply: " + valueOfSupply);
 		System.out.println("VAT: " + vat);
