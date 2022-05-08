156d5dc63dcbc8e972924a8a7b21e7da72ed21d2
min2oyo <min2u@outlook.com>
2022-05-08 12:35:37 +0900

    4.3 조건문 응용

--- HelloWorld/src/flowControl/AuthApp.java
+++ HelloWorld/src/flowControl/AuthApp.java
@@ -4,21 +4,18 @@ public class AuthApp {
 
 	public static void main(String[] args) {
 
-		String[] user = { "ria" };
+		String[] user = { "ria", "1111" };
 		String id = "ria";
+		String password = "1111";
 
 		System.out.println("Hi. ");
-		if (inputId(user).equals(id)) { // 문자열은 이거로 한다던데..?
-//		if (inputId(user) == id) {	// 이것도 true로 뜬다(?)
+		if (user[0].equals(id) && user[1].equals(password)) { // 문자열은 이거로 한다던데..?
+//		if (user[0] == id && user[1] == password) {	// 이것도 true로 뜬다(?)
 			System.out.println("Master!");
 		} else {
 			System.out.println("Who are you?");
 		}
 
-	}
-
-	public static String inputId(String[] user) {
-		return user[0];
 	}
 
 }
