bea730f7cafd941b4f4a7ade4ab00c0de701d303
min2oyo <min2u@outlook.com>
2022-05-08 10:46:02 +0900

    2. Boolean Datatype

--- HelloWorld/src/myApp/AccountingClassApp.java
+++ HelloWorld/src/myApp/AccountingClassApp.java
@@ -4,20 +4,20 @@ public class AccountingClassApp {
 
 	public static void main(String[] args) {
 
-		 // instance 
-        Accounting a1 = new Accounting();
-        a1.valueOfSupply = 10000.0;
-        a1.vatRate = 0.1;
-        a1.expenseRate = 0.3;
-        a1.print();
-         
-        Accounting a2 = new Accounting();
-        a2.valueOfSupply = 20000.0;
-        a2.vatRate = 0.05;
-        a2.expenseRate = 0.2;
-        a2.print();
-         
-        a1.print();
+		// instance
+		Accounting a1 = new Accounting();
+		a1.valueOfSupply = 10000.0;
+		a1.vatRate = 0.1;
+		a1.expenseRate = 0.3;
+		a1.print();
+
+		Accounting a2 = new Accounting();
+		a2.valueOfSupply = 20000.0;
+		a2.vatRate = 0.05;
+		a2.expenseRate = 0.2;
+		a2.print();
+
+		a1.print();
 
 	}
 
