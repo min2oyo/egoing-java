21d77a4cffef2176b6f3dc845225d1c977362040
min2oyo <min2u@outlook.com>
2022-05-08 13:07:34 +0900

    5. equals

--- HelloWorld/src/flowControl/AuthApp.java
+++ HelloWorld/src/flowControl/AuthApp.java
@@ -4,13 +4,14 @@ public class AuthApp {
 
 	public static void main(String[] args) {
 
-		String[] user = { "ria", "1111" };
+		String inputId = new String("ria");
+		String inputPs = new String("1111");
+
 		String id = "ria";
 		String password = "1111";
 
 		System.out.println("Hi. ");
-		if (user[0].equals(id) && user[1].equals(password)) { // 문자열은 이거로 한다던데..?
-//		if (user[0] == id && user[1] == password) {	// 이것도 true로 뜬다(?)
+		if (inputId.equals(id) && inputPs.equals(password)) {
 			System.out.println("Master!");
 		} else {
 			System.out.println("Who are you?");
