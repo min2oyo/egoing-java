531a6c4869ae79e27b6191d4b495e7af88c6ec76
min2oyo <min2u@outlook.com>
2022-05-09 17:58:39 +0900

    8.2 활용 | 인스턴스화

--- HelloWorld/src/objectOrientedProgramming/AccountingApp.java
+++ HelloWorld/src/objectOrientedProgramming/AccountingApp.java
@@ -4,11 +4,15 @@ public class AccountingApp {
 
 	public static void main(String[] args) {
 
-		Accounting.valueOfSupply = 10000.0;
+		Accounting a1 = new Accounting(10000.0);
+		System.out.println("Value of supply: " + a1.valueOfSupply);
+		System.out.println("VAT: " + a1.getVAT());
+		System.out.println("Total: " + a1.getTotal());
 
-		System.out.println("Value of supply : " + Accounting.valueOfSupply);
-		System.out.println("VAT : " + Accounting.getVAT());
-		System.out.println("Total : " + Accounting.getTotal());
+		Accounting a2 = new Accounting(20000.0);
+		System.out.println("Value of supply: " + a2.valueOfSupply);
+		System.out.println("VAT: " + a2.getVAT());
+		System.out.println("Total: " + a2.getTotal());
 
 	}
 
@@ -16,14 +20,18 @@ public class AccountingApp {
 
 class Accounting {
 
-	public static double valueOfSupply; // 공급가액
-	public static double vatRate = 0.1; // 부가가치세율
+	public double valueOfSupply; // 공급가액
+	public static double vatRate = 0.1; // 부가가치세율 // 어디에나 공통적인 값
 
-	public static double getVAT() { // 부가가치세
+	public Accounting(double valueOfSupply) {
+		this.valueOfSupply = valueOfSupply;
+	}
+
+	public double getVAT() { // 부가가치세
 		return valueOfSupply * vatRate;
 	}
 
-	public static double getTotal() { // 판매가
+	public double getTotal() { // 판매가
 		return valueOfSupply + getVAT();
 	}
 
