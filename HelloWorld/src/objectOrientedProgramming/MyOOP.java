fb3fab9f705f332ebdf0606026bd787ca5b43507
min2oyo <min2u@outlook.com>
2022-05-09 13:07:57 +0900

    4.1 클래스 | 존재 이유와 기본형식

--- HelloWorld/src/objectOrientedProgramming/MyOOP.java
+++ HelloWorld/src/objectOrientedProgramming/MyOOP.java
@@ -1,35 +1,39 @@
 package objectOrientedProgramming;
 
 public class MyOOP {
-	
-	public static void main(String[] args) {
-		
-		delimiter = "---";
-		printA();
-		printA();
-		printB();
-		printB();
 
-		delimiter = "***";
-		printA();
-		printA();
-		printB();
-		printB();
+	public static void main(String[] args) {
+
+		Print.delimiter = "---";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
+
+		Print.delimiter = "***";
+		Print.A();
+		Print.A();
+		Print.B();
+		Print.B();
 
 	}
+
+}
+
+class Print {
 	
 	public static String delimiter;
 
-	public static void printA() {
+	public static void A() {
 		System.out.println(delimiter);
 		System.out.println("A");
 		System.out.println("A");
 	}
 
-	public static void printB() {
+	public static void B() {
 		System.out.println(delimiter);
 		System.out.println("B");
 		System.out.println("B");
 	}
-
+	
 }
