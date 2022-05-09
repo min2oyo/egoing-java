e1dde7fd54122f82b2d312ba18a103b228ffec00
min2oyo <min2u@outlook.com>
2022-05-09 20:57:49 +0900

    2. 인터페이스 형식

--- HelloWorld/Java2/interface_/InterfaceApp.java
+++ HelloWorld/Java2/interface_/InterfaceApp.java
@@ -3,25 +3,37 @@ package interface_;
 public class InterfaceApp {
 
 	public static void main(String[] args) {
-
+		
 		RealCal c = new RealCal();
 		System.out.println(c.sum(2, 1));
-
+		c.print();
+		System.out.println(c.PI);
+	
 	}
 
 }
 
 interface Calculable {
 
+	double PI = 3.14;
 	int sum(int v1, int v2);
 
 }
 
-class RealCal implements Calculable {
+interface printable {
+	void print();
+}
+
+class RealCal implements Calculable, printable {
 
 	@Override
 	public int sum(int v1, int v2) {
 		return v1 + v2;
+	}
+
+	@Override
+	public void print() {
+		System.out.println("This is RealCal!!");
 	}
 
 }
