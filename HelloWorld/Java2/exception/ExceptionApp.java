425837f557f0c401b03dd571405f9f392d38ef1e
min2oyo <min2u@outlook.com>
2022-05-09 22:09:33 +0900

    3. 예외의 처리

--- HelloWorld/Java2/exception/ExceptionApp.java
+++ HelloWorld/Java2/exception/ExceptionApp.java
@@ -5,8 +5,19 @@ public class ExceptionApp {
 	public static void main(String[] args) {
 
 		System.out.println(1);
-		System.out.println(2 / 0);
-		System.out.println(3);
+		int[] scores = { 10, 20, 30 };
+		try {
+			System.out.println(2);
+			System.out.println(scores[3]);
+			System.out.println(3);
+			System.out.println(2 / 0);
+			System.out.println(4);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("없는 값을 찾고 계시네요 ^^");
+		} catch (ArithmeticException e) {
+			System.out.println("잘못된 계산이네요");
+		}
+		System.out.println(5);
 
 	}
 
