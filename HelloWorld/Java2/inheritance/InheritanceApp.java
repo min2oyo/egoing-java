d13860adc194f5017ce82161391edc5e0ea20ee1
min2oyo <min2u@outlook.com>
2022-05-09 19:33:56 +0900

    2. 기능의 개선과 발전

--- HelloWorld/Java2/inheritance/InheritanceApp.java
+++ HelloWorld/Java2/inheritance/InheritanceApp.java
@@ -6,10 +6,12 @@ public class InheritanceApp {
 
 		Cal1 c1 = new Cal1();
 		System.out.println(c1.sum(2, 1));
-		
-		Cal3 c3 = new Cal3();
-		System.out.println(c3.sum(2, 1));
-			
+
+		Cal2 c2 = new Cal2();
+		System.out.println(c2.sum(2, 1));
+		System.out.println(c2.minus(2, 1));
+		System.out.println(c2.sum(2, 1));
+
 	}
 
 }
@@ -20,16 +22,14 @@ class Cal1 {
 	}
 }
 
-class Cal2 {
-	public int sum(int v1, int v2) {
-		return v1 + v2;
-	}
-
+class Cal2 extends Cal1 {
 	public int minus(int v1, int v2) {
 		return v1 - v2;
 	}
-}
 
-class Cal3 extends Cal1 {
-
+	@Override
+	public int sum(int v1, int v2) {
+		System.out.println("Cal2!!!");
+		return v1 + v2;
+	}
 }
