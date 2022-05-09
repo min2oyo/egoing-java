22f642a0380659d962bb20561cb5931c96fe5d72
min2oyo <min2u@outlook.com>
2022-05-09 20:15:45 +0900

    5. 상속과 생성자

--- HelloWorld/Java2/inheritance/InheritanceApp.java
+++ HelloWorld/Java2/inheritance/InheritanceApp.java
@@ -4,13 +4,11 @@ public class InheritanceApp {
 
 	public static void main(String[] args) {
 
-		Cal1 c1 = new Cal1();
-		System.out.println(c1.sum(2, 1));
-
-		Cal2 c2 = new Cal2();
-		System.out.println(c2.sum(2, 1));
-		System.out.println(c2.minus(2, 1));
-		System.out.println(c2.sum(2, 1));
+		Cal1 c1 = new Cal1(2, 1);
+		Cal2 c2 = new Cal2(2, 1);
+		System.out.println(c1.sum()); // 3
+		System.out.println(c2.sum()); // 3
+		System.out.println(c2.minus()); // 1
 
 	}
 
@@ -18,27 +16,29 @@ public class InheritanceApp {
 
 class Cal1 {
 
-	public int sum(int v1, int v2) {
-		return v1 + v2;
+	int v1, v2;
+
+	public Cal1(int v1, int v2) {
+		System.out.println("Cal1 init!!");
+		this.v1 = v1;
+		this.v2 = v2;
 	}
 
-	// Overloading
-	public int sum(int v1, int v2, int v3) {
-		return this.sum(v1, v2) + v3;
+	public int sum() {
+		return this.v1 + this.v2;
 	}
 
 }
 
 class Cal2 extends Cal1 {
 
-	@Override
-	public int sum(int v1, int v2) {
-		System.out.println("Cal2!!!");
-		return super.sum(v1, v2);
+	public Cal2(int v1, int v2) {
+		super(v1, v2);
+		System.out.println("Cal3 init!!");
 	}
 
-	public int minus(int v1, int v2) {
-		return v1 - v2;
+	public int minus() {
+		return this.v1 - this.v2;
 	}
 
 }
